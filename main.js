17a3f5836c43e6b7936b49ec5cd81db3a1aab56e
Adi1512 <mj_murrell@hotmail.co.uk>
2026-04-07 17:43:40 +1000

    init commit

---
+++ main.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow, ipcMain } = require('electron')
+
+let win
+
+app.whenReady().then(() => {
+  win = new BrowserWindow({
+    width: 360,
+    height: 220,
+    alwaysOnTop: true,
+    frame: false,
+    transparent: true,
+    resizable: false,
+    skipTaskbar: false,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  })
+
+  win.loadFile('index.html')
+  win.setAlwaysOnTop(true, 'screen-saver')
+})
+
+ipcMain.on('resize-window', (e, { width, height }) => {
+  win.setSize(width, height)
+})
+
+ipcMain.on('move-window', (e, { x, y }) => {
+  const [wx, wy] = win.getPosition()
+  win.setPosition(wx + x, wy + y)
+})
+
+ipcMain.on('close-app', () => app.quit())
